4643e3aab5624bef1995ba8f075905637578f93b
SuganyaDhanapal <153515286+SuganyaDhanapal@users.noreply.github.com>
2023-12-11 17:56:39 +0530

    Add files via upload
    uploaded Day 2 assignments

---
+++ day2/Assignment1.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+	
+			ChromeDriver driver=new ChromeDriver(); 
+			
+			driver.get("http://leaftaps.com/opentaps/control/main");
+			}
+		
+
+	}
+
+}
